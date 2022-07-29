d497ad0599bdc2d6dd0348899cdf73be73f60075
wjdskagjs1 <wjdskagjs1@naver.com>
2022-07-29 19:32:35 +0900

    package installer update

--- 패키지 설치.bat
+++ 패키지 설치.bat
@@ -1,4 +1,4 @@
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-pip install -q transformers~=4.12.0
+pip install transformers
 pip install fastai
 pause
